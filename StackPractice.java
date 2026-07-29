2f9c221a5bb823a1a6b85d3475ff2a0c927fda0e
Axjer <axjer96@gmail.com>
2026-07-29 19:19:49 +0800

    add new content in stack

--- StackPractice.java
+++ StackPractice.java
@@ -4,17 +4,27 @@ public class StackPractice {
    public static void main(String[] args) {
       Stack<String> stack = new Stack<String>();
 
-      stack.push("1");
-      stack.push("2");
-      stack.push("3");
-      stack.push("4");
-      stack.push("5");
+      // stack(堆疊):後進先出(Last-In First-Out)的資料結構
+      // push():將資料新增至最頂端
+      // pop():從最頂端移除資料
 
-      String removeNum = stack.pop();
+      stack.push("Java");
+      stack.push("Python");
+      stack.push("JavaScript");
+      stack.push("HTML");
+      stack.push("CSS");
 
-      System.out.println(stack);
-      System.out.println(stack.peek());
-      System.out.println(removeNum);
-      System.out.println(stack.search("3"));
+      stack.pop(); // 移除"CSS"
+      stack.pop(); // 移除"HTML"
+
+      System.out.println(stack); // ["Java", "Python", "JavaScript"]
+      System.out.println(stack.peek()); // JavaScript，peek():回傳目前最上面那筆資料
+      System.out.println(stack.search("Python")); // 2，search():尋找並回傳指定元素在堆疊中的位置，找不到指定元素回傳-1
+
+      // stack(堆疊)的用途
+      // 1. 文字編輯器中的「復原／重做」功能 (Undo / Redo)
+      // 2. 瀏覽器歷史紀錄的「上一頁／下一頁」切換
+      // 3. 回溯演算法
+      // 4. 函式呼叫(call stack，呼叫堆疊)
    }
 }
